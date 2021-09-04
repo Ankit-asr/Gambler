74b247f50afe4afb524ce125e8055dd977ea51e5
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-04 10:25:21 +0530

    [Ankit] Add: Initials stake of player

---
+++ Gambler/Gambler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler
+{
+    class Gambler
+    {
+        const int TOTAL_STAKE = 100;
+        const int BET_EVERYDAY = 1;
+
+        public void Initials()
+        {
+            Console.WriteLine("Player Total Stake :" + TOTAL_STAKE);
+            Console.WriteLine("Player daily bet :" + BET_EVERYDAY);
+        }
+        public void Bet()
+        {
+            int Stake = 1;
+            Random random = new Random();
+            int player = random.Next(0, 2);
+            if (player == 0)
+            {
+                Console.WriteLine("Player Loss game");
+                Stake = Stake - 1;
+                Console.WriteLine("Amount after Losing bet "+Stake);
+            }
+            else
+            {
+                Console.WriteLine("Player Wins game");
+                Stake = Stake + 1;
+                Console.WriteLine("Amount after Winning bet " + Stake);
+
+            }
+
+        }
+    }
+}
---
+++ Gambler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambler Problem");
+            Gambler gambler = new Gambler();
+            gambler.Initials();
+            gambler.Bet();
+        }
+    }
+}
