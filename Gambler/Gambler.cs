ef3c65427242b0b272916d38b21f82911c53e025
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-04 10:55:51 +0530

    [Ankit] Add: Player bets and Wins or Lose game

--- Gambler/Gambler.cs
+++ Gambler/Gambler.cs
@@ -16,22 +16,23 @@ namespace Gambler
         }
         public void Bet()
         {
-            int Stake = 1;
+            int Stake = 0;
             Random random = new Random();
             int player = random.Next(0, 2);
             if (player == 0)
             {
                 Console.WriteLine("Player Loss game");
                 Stake = Stake - 1;
-                Console.WriteLine("Amount after Losing bet "+Stake);
+                //Console.WriteLine("Amount after Losing bet "+Stake);
             }
             else
             {
                 Console.WriteLine("Player Wins game");
                 Stake = Stake + 1;
-                Console.WriteLine("Amount after Winning bet " + Stake);
+                //Console.WriteLine("Amount after Winning bet " + Stake);
 
             }
+            Console.WriteLine("Total Stakes remaining with Player:"+(TOTAL_STAKE+Stake));
 
         }
     }
